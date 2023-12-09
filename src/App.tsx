fca797239928674e4c0eed376a6514288c54b275
Klychnikov Alexandr <klychnikov75@mail.ru>
2023-12-09 18:09:23 +0300

    feat: create block About

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import * as React from "react";
 import Header from "@components/header/header";
 import "./App.sass";
 import FavoriteCoffee from "@components/favorite-coffee/favorite-coffee";
+import About from "@components/about/about";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <FavoriteCoffee />
+      <About />
     </div>
   );
 }
---
+++ src/components/about/about.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import "./about.sass";
+
+const About = () => {
+  return (
+    <section className="about" id="about">
+      <h2 className="about__heading">
+        Resource is
+        <span className="about__accent">
+          &nbsp;the perfect and cozy place&nbsp;
+        </span>
+        where you can enjoy a variety of hot beverages, relax, catch up with
+        friends, or get some work done.
+      </h2>
+      <div className="about__images">
+        {["one", "two", "three", "four"].map((img) => (
+          <div className={`about__image ${img}`}></div>
+        ))}
+      </div>
+    </section>
+  );
+};
+export default About;
